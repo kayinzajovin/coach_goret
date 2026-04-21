3edddbb2bad30ab1d12a52cb2f87fc97ceffc117
jovinkayinza <kayinzajovin1@gmail.com>
2026-04-21 20:49:11 +0300

    task 3

--- OneDrive/Desktop/assignment_g/setup.py
+++ OneDrive/Desktop/assignment_g/setup.py
@@ -27,3 +27,19 @@ conn.commit()
 rows = conn.execute('SELECT * FROM products').fetchall()
 for r in rows:
     print(r)
+
+conn.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT DEFAULT 'attendant'
+)
+''')
+
+conn.execute("""
+INSERT OR IGNORE INTO users (username, password, role) VALUES
+('admin', 'admin123', 'admin'),
+('fatuma', 'pass456', 'attendant'),
+('wasswa', 'pass789', 'manager')
+""")
